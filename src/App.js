b8dc514b401ceb4b38819e3743c6ef8f62dc912c
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-03-09 18:02:06 +0200

    API IMPL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import React from "react";
 
+const API_URL = "http://www.omdbapi.com?apikey=a2226dc3"
+
 const App = () => {
+
+    const searchMovies = async () => {}
+    useEffect(() => {}, [])
+
     return (
         <h1>Hi</h1>
     );
