97db89281b74282f413821d1aa1d427af72cf120
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-03-10 12:09:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "http://www.omdbapi.com?apikey=a2226dc3";
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
